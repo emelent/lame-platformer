17fe8c5379c2755de830af47ef5b1175935be8a7
Roy <technicmk@gmail.com>
2018-02-21 07:50:58 +0200

    Turn off canvas antialiasing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-lame.game = new Phaser.Game(800, 640,  Phaser.AUTO, '')
-
+lame.game = new Phaser.Game(800, 640,  Phaser.AUTO, '', null, false, false)
 lame.game.state.add('bootState', lame.BootState)
 lame.game.state.add('loadState', lame.LoadState)
 lame.game.state.add('gameState', lame.GameState)
